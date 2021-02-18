ea84d394fad2a4a90633627f16da2f3d7952db55
edaozcan <73548267+edaozcan@users.noreply.github.com>
2021-02-19 01:51:12 +0300

    Add project files.

---
+++ Entities/Concrete/Brands.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    class Brands:IEntity
+    {
+        public int BrandsId { get; set; }
+        public int BrandsName { get; set; }
+
+    }
+}
---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Car:IEntity
+    {
+        public int CarId { get; set; }
+        public int BrandsId { get; set; }
+        public int ColorId { get; set; }
+        public int Stock { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
